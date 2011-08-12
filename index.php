b0faf863f0e5d30bfee71cefc5a91003c1523f41
Reynaldo <jamjrs@gmail.com>
2011-08-11 21:41:14 -0300

    Mostrando código de acesso no retorno

--- index.php
+++ index.php
@@ -4,3 +4,6 @@
 
 
 <a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&scope=email,read_stream">Autenticando Aplicação</a>
+
+
+<?php echo "Codigo de acesso \$code: $code <br>\n"; ?>
