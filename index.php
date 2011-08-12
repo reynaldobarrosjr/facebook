5e122ab1d4df2e9f6e1c2fdb58442ca520ae09f2
Reynaldo <jamjrs@gmail.com>
2011-08-11 21:21:13 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<h1>Server Side flow OA Auth Facebook - Teste</h1>
+
+
+<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=YOUR_URL&scope=email,read_stream">Autenticando Aplicação</a>
