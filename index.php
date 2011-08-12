ef4f0f701407e0214c4c5faaa472e8fe14df3c39
Reynaldo <jamjrs@gmail.com>
2011-08-11 21:34:21 -0300

    Adicionando a url de retorno

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <h1>Server Side flow OA Auth Facebook - Teste</h1>
 
+<pScript para autenticaçnao de uma aplicação para o Facebook. fluxo 1</a>
 
-<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=YOUR_URL&scope=email,read_stream">Autenticando Aplicação</a>
+
+<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://apps.facebook.com/amigosli&scope=email,read_stream">Autenticando Aplicação</a>
