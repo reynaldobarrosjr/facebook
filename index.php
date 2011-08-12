2c0552204ab732952c19d2a24118be383adf5fe9
Reynaldo <jamjrs@gmail.com>
2011-08-11 22:23:39 -0300

    Server side php incluido

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <pScript para autenticaçnao de uma aplicação para o Facebook. fluxo 1</a>
 
 
-<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&scope=email,read_stream">Autenticando Aplicação</a>
 
-<h2>C&oacute;digo de Autoriza&ccedil;&atilde;o</h2>
+
+<h2>Passo 1 - C&oacute;digo de Autoriza&ccedil;&atilde;o</h2>
+<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&scope=email,read_stream">Autenticando Aplicação</a>
 
 
 <?php echo "Codigo de acesso \$code: $code <br>\n"; ?>
@@ -13,8 +14,13 @@
 <?php echo $_REQUEST['code']; ?>
 
 
-<h2>Conseguindo o token</h2>
+<h2>Passo 2 - Conseguindo o token</h2>
 
 <a href="https://graph.facebook.com/oauth/access_token?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&client_secret=ec97fcf283708082d6ed945a36a14b49&code=<?php echo $_REQUEST['code'] ?>">Pegar Token</a>
 
-<p>https://graph.facebook.com/oauth/access_token?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&client_secret=ec97fcf283708082d6ed945a36a14b49&code=THE_CODE_FROM_ABOVE</p>
+<p>https://graph.facebook.com/oauth/access_token?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&client_secret=ec97fcf283708082d6ed945a36a14b49&code=<?php echo$_REQUEST['code']?></p>
+
+
+<h3>Passo 3 - Chamada a API</h3>
+
+<a href"https://graph.facebook.com/me/friends?access_token">Lista de amigos meus JSON</a>
