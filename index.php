6a5c1aa2324ab10eb3a9ad4446b3306cbaf151cd
Reynaldo <jamjrs@gmail.com>
2011-08-11 21:54:45 -0300

    Pegando token

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
 
 <a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&scope=email,read_stream">Autenticando Aplicação</a>
 
+<h2>Código de Autorização</h2>
+
 
 <?php echo "Codigo de acesso \$code: $code <br>\n"; ?>
 
 <?php echo $_REQUEST['code']; ?>
+
+
+<h2>Conseguindo o token</h2>
+
+<a href="https://graph.facebook.com/oauth/access_token?client_id=YOUR_APP_ID&redirect_uri=http://www.trevobr.com/facebook/&client_secret=ec97fcf283708082d6ed945a36a14b49&code=<?php echo $_REQUEST['code'] ?>">Pegar Token</a>
