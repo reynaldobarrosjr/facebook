e69f045e7897851c18dc5a14f578d86efd532057
Reynaldo <jamjrs@gmail.com>
2011-08-11 22:25:14 -0300

    Mudando o index para o exemplo server side

--- index.php
+++ index.php
@@ -1,26 +1,38 @@
-<h1>Server Side flow OA Auth Facebook - Teste</h1>
+<?php 
 
-<pScript para autenticaçnao de uma aplicação para o Facebook. fluxo 1</a>
+   $app_id = "145781085505624";
+   $app_secret = "ec97fcf283708082d6ed945a36a14b49";
+   $my_url = "http://www.trevobr.com/facebook/serverside.php";
 
+   session_start();
+   $code = $_REQUEST["code"];
 
+   if(empty($code)) {
+     $_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
+     $dialog_url = "http://www.facebook.com/dialog/oauth?client_id=" 
+       . $app_id . "&redirect_uri=" . urlencode($my_url) . "&state="
+       . $_SESSION['state'];
 
+     echo("<script> top.location.href='" . $dialog_url . "'</script>");
+   }
 
-<h2>Passo 1 - C&oacute;digo de Autoriza&ccedil;&atilde;o</h2>
-<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&scope=email,read_stream">Autenticando Aplicação</a>
+   if($_REQUEST['state'] == $_SESSION['state']) {
+     $token_url = "https://graph.facebook.com/oauth/access_token?"
+       . "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url)
+       . "&client_secret=" . $app_secret . "&code=" . $code;
 
+     $response = file_get_contents($token_url);
+     $params = null;
+     parse_str($response, $params);
 
-<?php echo "Codigo de acesso \$code: $code <br>\n"; ?>
+     $graph_url = "https://graph.facebook.com/me?access_token=" 
+       . $params['access_token'];
 
-<?php echo $_REQUEST['code']; ?>
+     $user = json_decode(file_get_contents($graph_url));
+     echo("Hello " . $user->name);
+   }
+   else {
+     echo("The state does not match. You may be a victim of CSRF.");
+   }
 
-
-<h2>Passo 2 - Conseguindo o token</h2>
-
-<a href="https://graph.facebook.com/oauth/access_token?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&client_secret=ec97fcf283708082d6ed945a36a14b49&code=<?php echo $_REQUEST['code'] ?>">Pegar Token</a>
-
-<p>https://graph.facebook.com/oauth/access_token?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&client_secret=ec97fcf283708082d6ed945a36a14b49&code=<?php echo$_REQUEST['code']?></p>
-
-
-<h3>Passo 3 - Chamada a API</h3>
-
-<a href"https://graph.facebook.com/me/friends?access_token">Lista de amigos meus JSON</a>
+ ?>
---
+++ index_back.php
@@ -0,0 +1,26 @@
+<h1>Server Side flow OA Auth Facebook - Teste</h1>
+
+<pScript para autenticaçnao de uma aplicação para o Facebook. fluxo 1</a>
+
+
+
+
+<h2>Passo 1 - C&oacute;digo de Autoriza&ccedil;&atilde;o</h2>
+<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&scope=email,read_stream">Autenticando Aplicação</a>
+
+
+<?php echo "Codigo de acesso \$code: $code <br>\n"; ?>
+
+<?php echo $_REQUEST['code']; ?>
+
+
+<h2>Passo 2 - Conseguindo o token</h2>
+
+<a href="https://graph.facebook.com/oauth/access_token?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&client_secret=ec97fcf283708082d6ed945a36a14b49&code=<?php echo $_REQUEST['code'] ?>">Pegar Token</a>
+
+<p>https://graph.facebook.com/oauth/access_token?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&client_secret=ec97fcf283708082d6ed945a36a14b49&code=<?php echo$_REQUEST['code']?></p>
+
+
+<h3>Passo 3 - Chamada a API</h3>
+
+<a href"https://graph.facebook.com/me/friends?access_token">Lista de amigos meus JSON</a>
