fb60dfc8078158cd53b42daebb8353ba34770b0c
Reynaldo <jamjrs@gmail.com>
2011-08-11 21:37:45 -0300

    Mudando a URL de retorno para o dominio da trevo

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 <pScript para autenticaçnao de uma aplicação para o Facebook. fluxo 1</a>
 
 
-<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://apps.facebook.com/amigosli&scope=email,read_stream">Autenticando Aplicação</a>
+<a href="https://www.facebook.com/dialog/oauth?client_id=145781085505624&redirect_uri=http://www.trevobr.com/facebook/index.php&scope=email,read_stream">Autenticando Aplicação</a>
